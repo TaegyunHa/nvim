a1ac2329bbee3db1832fb79890c369f990995f97
Taegyun Ha <34822734+TaegyunHa@users.noreply.github.com>
2023-08-25 21:36:12 +0100

    add more remap

--- lua/taegyun/remap.lua
+++ lua/taegyun/remap.lua
@@ -1,2 +1,50 @@
 vim.g.mapleader = " "
+
+-- Go out to directory tree
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Move selected block up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Keep cursor at the begining of the line
+vim.keymap.set("n", "J", "mzJ'z")
+
+-- Keep cursor at the middle
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Keep vim clipboard after paste in block
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- Yank into system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- Cut into system clipboard
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+-- Disable Q
+vim.keymap.set("n", "Q", "<nop>")
+
+-- Quick fix 
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- Replace all same words on cursor
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Change current file to excutable
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- Source curront file
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
+end)
+
