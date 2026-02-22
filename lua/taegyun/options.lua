53b1db0473afa39e16dd3c9947f0bead6efa8d52
Taegyun Ha <taegyunha92@gmail.com>
2026-02-22 18:12:15 +0000

    update option

--- lua/taegyun/options.lua
+++ lua/taegyun/options.lua
@@ -35,11 +35,11 @@ vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 10
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
-vim.opt.updatetime = 50
+vim.opt.updatetime = 250
 
 -- Whitespace characters
 vim.opt.list = true
