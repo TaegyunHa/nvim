8a1d141e0b4c1d6bde9009352101e0329441dbc9
Taegyun Ha <taegyunha92@gmail.com>
2026-03-20 22:25:00 +0000

    update blink key-bind

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -27,7 +27,16 @@ return {
 	--- @module 'blink.cmp'
 	--- @type blink.cmp.Config
 	opts = {
-		keymap = { preset = "default" },
+		keymap = {
+			preset = "none",
+			["<C-j>"] = { "select_next", "fallback" },
+			["<C-k>"] = { "select_prev", "fallback" },
+			["<Tab>"] = { "accept", "fallback" },
+			["<Escape>"] = { "cancel", "fallback" },
+			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+			["<C-b>"] = { "scroll_documentation_up", "fallback" },
+			["<C-f>"] = { "scroll_documentation_down", "fallback" },
+		},
 		appearance = { nerd_font_variant = "mono" },
 		completion = {
 			documentation = { auto_show = true, auto_show_delay_ms = 500 },
